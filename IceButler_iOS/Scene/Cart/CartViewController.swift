131d55299a450b085c39bc866cc3263136912c2f
ryr0121 <ryr0121@gmail.com>
2023-03-30 15:42:53 +0900

    feat: 하단 탭바 구성

---
+++ IceButler_iOS/Scene/Cart/CartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CartViewController.swift
+//  IceButler_iOS
+//
+//  Created by 김초원 on 2023/03/30.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ IceButler_iOS/Scene/DefaultTabBarController.swift
@@ -0,0 +1,76 @@
+//
+//  DefaultTabBarController.swift
+//  IceButler_iOS
+//
+//  Created by 김초원 on 2023/03/30.
+//
+
+import UIKit
+
+class DefaultTabBarController: UITabBarController {
+    
+    private let homeTab = UITabBarItem(title: nil, image: UIImage(named: "main"), selectedImage: UIImage(named: "main")) // TODO: .fill 아이콘으로 변경
+    private let recipeTab = UITabBarItem(title: nil, image: UIImage(named: "recipe"), selectedImage: UIImage(named: "recipe.fill"))
+    private let cartTab = UITabBarItem(title: nil, image: UIImage(named: "cart"), selectedImage: UIImage(named: "cart.fill"))
+    private let mypageTab = UITabBarItem(title: nil, image: UIImage(named: "mypage"), selectedImage: UIImage(named: "mypage.fill"))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBar()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        var tabFrame = tabBar.frame
+        if UIDevice.hasNotch {
+            tabFrame.size.height = 100
+            tabFrame.origin.y = self.view.frame.size.height - 80
+        } else {
+            tabFrame.size.height = 60
+            tabFrame.origin.y = self.view.frame.size.height - 50
+        }
+        tabBar.frame = tabFrame
+        setLayoutTabBar()
+    }
+    
+    private func setLayoutTabBar() {
+        tabBar.layer.borderWidth = 0.5
+        tabBar.layer.borderColor = UIColor.blue.cgColor // TODO: 디자인 확정되면 border color 수정 예정
+        tabBar.clipsToBounds = true
+        
+        tabBar.unselectedItemTintColor = .black
+        tabBar.tintColor = .black
+        tabBar.barTintColor = .white
+    }
+    
+    private func configureTabBar() {
+        self.tabBarController?.tabBar.tintColor = .black
+        
+        // TODO: 레시피, 마이페이지 관련 화면 생성 후 instatiate 관련 내용 수정
+        var storyboard = UIStoryboard.init(name: "Basket", bundle: nil)
+        guard let homeViewController = storyboard.instantiateViewController(withIdentifier: "BasketViewController") as? BasketViewController else { return }
+        let home = UINavigationController(rootViewController: homeViewController)
+        home.tabBarItem = homeTab
+        
+        storyboard = UIStoryboard.init(name: "Basket", bundle: nil)
+        guard let recipeViewController = storyboard.instantiateViewController(withIdentifier: "BasketViewController") as? BasketViewController else { return }
+        let recipe = UINavigationController(rootViewController: recipeViewController)
+        recipe.tabBarItem = recipeTab
+        
+        storyboard = UIStoryboard.init(name: "Cart", bundle: nil)
+        guard let cartViewController = storyboard.instantiateViewController(withIdentifier: "CartViewController") as? CartViewController else { return }
+        let cart = UINavigationController(rootViewController: cartViewController)
+        cart.tabBarItem = cartTab
+        
+        storyboard = UIStoryboard.init(name: "Basket", bundle: nil)
+        guard let mypageViewController = storyboard.instantiateViewController(withIdentifier: "BasketViewController") as? BasketViewController else { return }
+        let mypage = UINavigationController(rootViewController: mypageViewController)
+        mypage.tabBarItem = mypageTab
+        
+        viewControllers = [
+            home,
+            recipe,
+            cart,
+            mypage
+        ]
+    }
+}
