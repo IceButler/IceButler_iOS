01ab0130552e8a4fbfd8c897b64fdef7dbf8e6f5
차유상 <yoo7969@naver.com>
2023-04-03 15:25:29 +0900

    feat: API 연결 준비

---
+++ IceButler_iOS/Network/APIManger.swift
@@ -0,0 +1,84 @@
+//
+//  APIManger.swift
+//  IceButler_iOS
+//
+//  Created by 유상 on 2023/04/03.
+//
+
+import Foundation
+import Alamofire
+
+private let BASE_URL = "http://localhost:8080"
+
+class APIManger {
+    static let shared = APIManger()
+    
+    func getData<T: Codable, U: Decodable>(urlEndpointString: String,
+                                           responseDataType: U.Type,
+                                           requestDataType: T.Type,
+                                           parameter: T?,
+                                           completionHandler: @escaping (GeneralResponseModel<U>)->Void) {
+        
+        guard let url = URL(string: BASE_URL + urlEndpointString) else { return }
+        
+        AF
+            .request(url, method: .get, parameters: parameter, headers: nil)
+            .responseDecodable(of: GeneralResponseModel<U>.self) { response in
+                print(response)
+                switch response.result {
+                case .success(let success):
+                    completionHandler(success)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            .resume()
+    }
+    
+    
+    
+    func postData<T: Codable, U: Decodable>(urlEndpointString: String,
+                                            responseDataType: U.Type,
+                                            requestDataType: T.Type,
+                                            parameter: T?,
+                                            completionHandler: @escaping (GeneralResponseModel<U>)->Void) {
+        
+        guard let url = URL(string: BASE_URL + urlEndpointString) else { return }
+        
+        AF
+            .request(url, method: .post, parameters: parameter, encoder: .json, headers: nil)
+            .responseDecodable(of: GeneralResponseModel<U>.self) { response in
+                print(response)
+                switch response.result {
+                case .success(let success):
+                    completionHandler(success)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            .resume()
+    }
+    
+    func putData<T: Codable, U: Decodable>(urlEndpointString: String,
+                                            responseDataType: U.Type,
+                                            requestDataType: T.Type,
+                                            parameter: T?,
+                                            completionHandler: @escaping (GeneralResponseModel<U>)->Void) {
+        
+        guard let url = URL(string: BASE_URL + urlEndpointString) else { return }
+        
+        AF
+            .request(url, method: .put, parameters: parameter, encoder: .json, headers: nil)
+            .responseDecodable(of: GeneralResponseModel<U>.self) { response in
+                print(response)
+                switch response.result {
+                case .success(let success):
+                    completionHandler(success)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            .resume()
+    }
+}
+
