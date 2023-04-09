60f2398f9654de51cd28b286ce70257de5ad28d6
chayoosang <yoo7969@naver.com>
2023-04-10 02:19:58 +0900

    #8 feat: 냉장고 음식 상세 조회 API 연결

---
+++ IceButler_iOS/Data/Service/래ㅐㅇ/FoodService.swift
@@ -0,0 +1,8 @@
+//
+//  FoodService.swift
+//  IceButler_iOS
+//
+//  Created by 유상 on 2023/04/10.
+//
+
+import Foundation
---
+++ IceButler_iOS/Presentation/ViewModel/Food/FoodViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FoodViewModel.swift
+//  IceButler_iOS
+//
+//  Created by 유상 on 2023/04/10.
+//
+
+import Foundation
