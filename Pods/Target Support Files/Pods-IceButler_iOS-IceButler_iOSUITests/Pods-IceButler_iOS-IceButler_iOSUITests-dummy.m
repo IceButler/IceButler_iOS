d25fa919c0e7c97fa97db1e56a1cb41285624c82
차유상 <81149634+chayoosang@users.noreply.github.com>
2023-04-18 01:13:11 +0900

    Delete Pods directory
    delete: Pods directory

--- Pods/Target Support Files/Pods-IceButler_iOS-IceButler_iOSUITests/Pods-IceButler_iOS-IceButler_iOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IceButler_iOS_IceButler_iOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IceButler_iOS_IceButler_iOSUITests
-@end
--- Pods/Target Support Files/Pods-IceButler_iOS/Pods-IceButler_iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IceButler_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_IceButler_iOS
-@end
