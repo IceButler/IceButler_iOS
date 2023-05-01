3fb9923142f593efebf305cc5523912609535794
Nya128 <nanalovesbap@gmail.com>
2023-05-02 04:28:39 +0900

    #79 fix: 레시피 메인 탭바 넘길 때 즐겨찾기 데이터 반영 안되는 문제 해결

--- IceButler_iOS/Presentation/ViewControllers/Recipe/Category/RecipeInFridgeViewController.swift
+++ IceButler_iOS/Presentation/ViewControllers/Recipe/Category/RecipeInFridgeViewController.swift
@@ -58,11 +58,11 @@ extension RecipeInFridgeViewController: UICollectionViewDelegate, UICollectionVi
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCollectionViewCell", for: indexPath) as! RecipeCollectionViewCell
         
         RecipeViewModel.shared.getFridgeRecipeCellInfo(index: indexPath.row) { recipe in
-            cell.setImage(imageUrl: recipe.recipeImgUrl)
-            cell.setName(name: recipe.recipeName)
-            cell.setCategory(category: recipe.recipeCategory)
-            cell.setPercent(percent: recipe.percentageOfFood)
-            cell.setLikeStatus(status: recipe.recipeLikeStatus)
+            cell.setImage(imageUrl: recipe!.recipeImgUrl)
+            cell.setName(name: recipe!.recipeName)
+            cell.setCategory(category: recipe!.recipeCategory)
+            cell.setPercent(percent: recipe!.percentageOfFood)
+            cell.setLikeStatus(status: recipe!.recipeLikeStatus)
         }
         
         return cell
