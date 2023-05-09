4ac1063e73fb231f6c087aadb366cb0748a6b0a0
Nya128 <nanalovesbap@gmail.com>
2023-05-09 19:32:00 +0900

    #83 remove: 불필요 코드 삭제

--- IceButler_iOS/Presentation/ViewControllers/Recipe/AddRecipeViewController.swift
+++ IceButler_iOS/Presentation/ViewControllers/Recipe/AddRecipeViewController.swift
@@ -192,7 +192,6 @@ class AddRecipeViewController: UIViewController {
                 self.categoryViewHeight.priority = UILayoutPriority(250)
                 self.categoryOpenImageView.image = UIImage(named: "recipeCategoryCloseIcon")
             }
-//            self.scrollView.updateContentSize()
         }
         isOpenCategoryView.toggle()
     }
@@ -201,10 +200,8 @@ class AddRecipeViewController: UIViewController {
         if !ingredientNameTextField.text!.isEmpty && !ingredientAmountTextField.text!.isEmpty {
             addedIngredientList.append([ingredientNameTextField.text!, ingredientAmountTextField.text!])
             ingredientStackViewTopConstraintToIngredientTableView.priority = UILayoutPriority(1000)
-            print(addedIngredientList)
             
             ingredientTableView.reloadData()
-//            scrollView.updateContentSize()
             scrollView.invalidateIntrinsicContentSize()
         }
     }
@@ -306,27 +303,6 @@ extension AddRecipeViewController: UITableViewDelegate, UITableViewDataSource {
         default:
             return
         }
-    }
-}
-
-extension UIScrollView {
-    func updateContentSize() {
-        let unionCalculatedTotalRect = recursiveUnionInDepthFor(view: self)
-        
-        // 계산된 크기로 컨텐츠 사이즈 설정
-        self.contentSize = CGSize(width: self.frame.width, height: unionCalculatedTotalRect.height+50)
-    }
-    
-    private func recursiveUnionInDepthFor(view: UIView) -> CGRect {
-        var totalRect: CGRect = .zero
-        
-        // 모든 자식 View의 컨트롤의 크기를 재귀적으로 호출하며 최종 영역의 크기를 설정
-        for subView in view.subviews {
-            totalRect = totalRect.union(recursiveUnionInDepthFor(view: subView))
-        }
-        
-        // 최종 계산 영역의 크기를 반환
-        return totalRect.union(view.frame)
     }
 }
 
