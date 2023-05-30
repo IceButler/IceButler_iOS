29c739b75522a49861bdc0e9347f4365a297f7c7
chayoosang <yoo7969@naver.com>
2023-05-30 18:59:16 +0900

    #152 fix: 마이페이지 결제 삭제

--- IceButler_iOS/Presentation/ViewControllers/MyPage/MyPageViewController.swift
+++ IceButler_iOS/Presentation/ViewControllers/MyPage/MyPageViewController.swift
@@ -13,15 +13,13 @@ class MyPageViewController: UIViewController {
     
     private let iconImgNameList: [[String]] = [
         ["myFridges", "myRecipe"],    /// 마이 냉장고, 마이 레시피
-        ["proVersion"],   /// 프로버전
         ["logout", "signout"],    /// 로그아웃, 회원탈퇴
         ["tos", "privatePolicy"] /// 약관안내, 개인정보처리방침
     ]
     
-    private let sectionList = ["My", "내 결제", "계정 설정", "앱 정보"]
+    private let sectionList = ["My", "계정 설정", "앱 정보"]
     private let menuList = [
         ["마이 냉장고", "마이 레시피"],
-        ["Pro 버전"],
         ["로그아웃", "회원탈퇴"],
         ["약관 안내", "개인 정보 처리 방침"]
     ]
@@ -174,8 +172,6 @@ extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
             default: return
             }
         case 1:
-            print("Pro 결제 화면으로 전환")
-        case 2:
             switch indexPath.row {
             case 0:
                 let alertVC = UIStoryboard(name: "Alert", bundle: nil).instantiateViewController(identifier: "AlertViewController") as! AlertViewController
@@ -198,7 +194,7 @@ extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
                 self.present(alertVC, animated: true)
             default: return
             }
-        case 3:
+        case 2:
             switch indexPath.row {
             case 0:
                 showPolicyWebVC(policyType: .tosGuide)
