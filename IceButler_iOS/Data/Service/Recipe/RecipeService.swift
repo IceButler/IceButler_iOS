9cd9ac111737345720b4dd6ffc117c24e2edabe9
Nya128 <nanalovesbap@gmail.com>
2023-05-31 16:32:44 +0900

    #147 fix: 레시피 response data값 nil일 경우 컬렉션뷰 footer 남아있는 현상 해결

--- IceButler_iOS/Data/Service/Recipe/RecipeService.swift
+++ IceButler_iOS/Data/Service/Recipe/RecipeService.swift
@@ -11,56 +11,56 @@ import Alamofire
 class RecipeService {
     var PAGING_SIZE: Int = 12
     
-    func getFridgeRecipes(fridgeType: FridgeType, fridgeIdx: Int, pageNumberToLoad: Int, completion: @escaping (RecipeResponseModel?) -> Void) {
+    func getFridgeRecipes(fridgeType: FridgeType, fridgeIdx: Int, pageNumberToLoad: Int, completion: @escaping (GeneralResponseModel<RecipeResponseModel>?) -> Void) {
         let parameter: Parameters = ["category" : "냉장고", "page" : pageNumberToLoad, "size" : PAGING_SIZE]
         switch fridgeType {
         case .homeUse:
             print("\(fridgeIdx) - 가정용")
             APIManger.shared.getRecipeData(urlEndpointString: "/recipes/\(fridgeIdx)", responseDataType: RecipeResponseModel.self, parameter: parameter) { response in
-                completion(response.data)
+                completion(response)
             }
         case .multiUse:
             print("\(fridgeIdx) - 공용")
             APIManger.shared.getRecipeData(urlEndpointString: "/multiRecipes/\(fridgeIdx)", responseDataType: RecipeResponseModel.self, parameter: parameter) { response in
-                completion(response.data)
+                completion(response)
             }
         }
     }
     
-    func getPopularRecipes(fridgeType: FridgeType, fridgeIdx: Int, pageNumberToLoad: Int, completion: @escaping (RecipeResponseModel?) -> Void) {
+    func getPopularRecipes(fridgeType: FridgeType, fridgeIdx: Int, pageNumberToLoad: Int, completion: @escaping (GeneralResponseModel<RecipeResponseModel>?) -> Void) {
         let parameter: Parameters = ["category" : "인기", "page" : pageNumberToLoad, "size" : PAGING_SIZE]
         switch fridgeType {
         case .homeUse:
             print("\(fridgeIdx) - 가정용")
             APIManger.shared.getRecipeData(urlEndpointString: "/recipes/\(fridgeIdx)", responseDataType: RecipeResponseModel.self, parameter: parameter) { response in
-                completion(response.data)
+                completion(response)
             }
         case .multiUse:
             print("\(fridgeIdx) - 공용")
             APIManger.shared.getRecipeData(urlEndpointString: "/multiRecipes/\(fridgeIdx)", responseDataType: RecipeResponseModel.self, parameter: parameter) { response in
-                completion(response.data)
+                completion(response)
             }
         }
     }
     
-    func getBookmarkRecipes(fridgeType: FridgeType, fridgeIdx: Int, pageNumberToLoad: Int, completion: @escaping (RecipeResponseModel?) -> Void) {
+    func getBookmarkRecipes(fridgeType: FridgeType, fridgeIdx: Int, pageNumberToLoad: Int, completion: @escaping (GeneralResponseModel<RecipeResponseModel>?) -> Void) {
         let parameter: Parameters = ["page" : pageNumberToLoad, "size" : PAGING_SIZE]
         switch fridgeType {
         case .homeUse:
             APIManger.shared.getRecipeData(urlEndpointString: "/recipes/\(fridgeIdx)/bookmark", responseDataType: RecipeResponseModel.self, parameter: parameter) { response in
-                completion(response.data)
+                completion(response)
             }
         case .multiUse:
             APIManger.shared.getRecipeData(urlEndpointString: "/multiRecipes/\(fridgeIdx)/bookmark", responseDataType: RecipeResponseModel.self, parameter: parameter) { response in
-                completion(response.data)
+                completion(response)
             }
         }
     }
     
-    func getMyRecipes(pageNumberToLoad: Int, completion: @escaping (MyRecipeResponseModel?) -> Void) {
+    func getMyRecipes(pageNumberToLoad: Int, completion: @escaping (GeneralResponseModel<MyRecipeResponseModel>?) -> Void) {
         let parameter: Parameters = ["page" : pageNumberToLoad, "size" : PAGING_SIZE]
         APIManger.shared.getRecipeData(urlEndpointString: "/recipes/myrecipe", responseDataType: MyRecipeResponseModel.self, parameter: parameter) { response in
-            completion(response.data)
+            completion(response)
         }
     }
     
