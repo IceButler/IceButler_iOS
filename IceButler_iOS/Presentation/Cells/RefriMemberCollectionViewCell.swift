779bcf0103433a5cc923e58e60087d713044b672
ryr0121 <ryr0121@gmail.com>
2023-06-03 16:27:41 +0900

    fix: 마이냉장고 조회 시 프로필 사진 없는 유저의 경우 기본 이미지 적용

--- IceButler_iOS/Presentation/Cells/RefriMemberCollectionViewCell.swift
+++ IceButler_iOS/Presentation/Cells/RefriMemberCollectionViewCell.swift
@@ -11,9 +11,8 @@ import Kingfisher
 class RefriMemberCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var crownImg: UIImageView!
-//    @IBOutlet weak var imgView: UIImageView!
     
-    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var profileImgView: UIImageView!
     @IBOutlet weak var nickname: UILabel!
     
     override func awakeFromNib() {
@@ -22,30 +21,33 @@ class RefriMemberCollectionViewCell: UICollectionViewCell {
     
     public func setupLayout() {
         crownImg.isHidden = true
-        imgView.layer.borderWidth = 0
+        profileImgView.layer.borderWidth = 0
         nickname.font = .systemFont(ofSize: 10, weight: .regular)
-//        imgView.tintColor = UIColor(red: 194/255, green: 210/255, blue: 227/255, alpha: 1)
-//        imgView.backgroundColor = UIColor(red: 41/255, green: 90/255, blue: 153/255, alpha: 1)
-        imgView.image = UIImage(named: "person.fill")
-        imgView.translatesAutoresizingMaskIntoConstraints = false
-        imgView.layer.cornerRadius = imgView.frame.width / 2 - 2
+        profileImgView.layer.cornerRadius = profileImgView.frame.width / 2 - 2
     }
     
     public func setupMainMemberProfile() {
         crownImg.isHidden = false
-        imgView.layer.cornerRadius = imgView.frame.width / 2 - 2
-        imgView.layer.borderColor = UIColor.signatureLightBlue.cgColor
-        imgView.layer.borderWidth = 3.0
+        profileImgView.layer.cornerRadius = profileImgView.frame.width / 2 - 2
+        profileImgView.layer.borderColor = UIColor.signatureLightBlue.cgColor
+        profileImgView.layer.borderWidth = 3.0
         nickname.font = .systemFont(ofSize: 10, weight: .heavy)
     }
 
     public func configure(data: FridgeUser) {
         nickname.text = data.nickname
         if let imgUrlStr = data.profileImgUrl {
-            let url = URL(string: imgUrlStr)
-            imgView.kf.setImage(with: url)
+//            let url = URL(string: imgUrlStr)
+//            profileImgView.kf.setImage(with: url)
+            // TODO: 프로필 사진 미설정 유저인 경우에 profileImgUrl가 Null값으로 반환되도록 서버 수정되면 이하 if문은 삭제 예정
+            if imgUrlStr == "https://ice-bulter-iamge-bucket.s3.ap-northeast-2.amazonaws.com/" {
+                profileImgView.image = UIImage(named: "defaultProfile")
+            } else {
+                let url = URL(string: imgUrlStr)
+                profileImgView.kf.setImage(with: url)
+            }
         }
-        else { imgView.image = UIImage(named: "person.fill") }
+        else { profileImgView.image = UIImage(named: "defaultProfile") }
         if data.role == "OWNER" { setupMainMemberProfile()  }
         else { crownImg.isHidden = true }
     }
