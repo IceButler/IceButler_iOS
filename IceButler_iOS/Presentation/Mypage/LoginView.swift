3d71b3f7710cf3e3d4da759015c826dd41ea26b2
차유상 <81149634+chayoosang@users.noreply.github.com>
2024-07-03 22:48:52 +0900

    feat: 디자인 시스템 컬러 추가

--- IceButler_iOS/Presentation/Mypage/LoginView.swift
+++ IceButler_iOS/Presentation/Mypage/LoginView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Image("iceButlerMainIcon")
+                .padding(.top, 141)
+            
+            Text("냉장고를 지켜주는 나만의 집사")
+            Text("냉집사")
+            
+        }.frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .top
+        )
+        .background(Color("Blue2"))
     }
 }
 
